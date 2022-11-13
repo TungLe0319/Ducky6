1d17a0ededabf7081d2f70787daa38ddc7eb9155
Tung N Le <tung.le0319@gmail.com>
2022-11-13 08:12:08 -0700

    Render Set Up

--- Ducky6.client/vite.config.js
+++ Ducky6.client/vite.config.js
@@ -5,7 +5,7 @@ import { defineConfig } from 'vite'
 export default defineConfig({
   plugins: [vue()],
   build: {
-    outDir: 'docs',
+    outDir: '../Ducky6/client',
     sourcemap: false
   },
   server: {
